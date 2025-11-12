b221d4faa242fb0d07206775ec3a1c19800d3510
한진희 <0200134hjh@gmail.com>
2025-11-12 19:47:16 +0900

    Create Hello.java

---
+++ src/Hello.java
@@ -0,0 +1,8 @@
+class Hello {
+    int x = 10;
+    void main() {
+        System.out.println("Running meaning-level Java in C++!");
+        int y = 3;
+        System.out.println("Sum: " + y);
+    }
+}
